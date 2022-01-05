e74031d3a0d5ce3b6a73297087507600bb969092
unoriginalak <aks.lingayat@gmail.com>
2022-01-04 21:44:59 -0500

    Enhancements1 Fixed website title, project image, home images, resume pdf viewer, removed footer, updated readme

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -9,7 +9,7 @@ import editor from "../../Assets/Projects/codeEditor.png";
 import chatify from "../../Assets/Projects/chatify.png";
 import suicide from "../../Assets/Projects/suicide.png";
 import bitsOfCode from "../../Assets/Projects/blog.png";
-import meetup from "../../Assets/Projects/blog.png";
+import meetup from "../../Assets/Projects/meetup.png";
 
 function Projects() {
   return (
